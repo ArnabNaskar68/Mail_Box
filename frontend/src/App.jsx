b69f00d4fa45d811d6d2b011ed28da7857d9b009
ArnabNaskar <arnabnaskar68@gmail.com>
2025-07-05 17:23:05 +0530

    modified file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,10 @@
 import { useState } from 'react'
 
 export default function App(){
-function Square({ value, onClick }) {
-  return <button onClick={onClick}>{value}</button>;
-}
 
-function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-
-  function handleClick(i) {
-    if (squares[i]) return;
-    const newSquares = [...squares];
-    newSquares[i] = xIsNext ? "X" : "O";
-    setSquares(newSquares);
-    setXIsNext(!xIsNext);
-  }
 
   return (
-    <div>
-      {squares.map((val, i) => (
-        <Square key={i} value={val} onClick={() => handleClick(i)} />
-      ))}
-    </div>
-  );
-}
-return <Board/>;
+<></>
+  )
 }
 
